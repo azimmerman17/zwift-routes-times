bf3ad9702e0fcfcbc35ea6124b2f153867794d9d
Andrew Zimmerman <azimmerman@pga.com>
2023-11-24 08:36:58 -0600

    Added comments to access token and activity list functions

--- backend/functions/getAccessToken.js
+++ backend/functions/getAccessToken.js
@@ -3,13 +3,16 @@ const axios = require('axios')
 require('dotenv').config()
 
 async function getAccessToken() {
-  const tokenExchangeUrl = 'https://www.strava.com/oauth/token'
-
+  // env varibles 
   const client_id = process.env.STRAVA_CLIENT_ID
   const client_secret = process.env.STRAVA_CLIENT_SECERT
   const refresh_token = process.env.STRAVA_RF_TOKEN
+  
+  // url for the token exchange
+  const tokenExchangeUrl = 'https://www.strava.com/oauth/token'
+  const params = `?client_id=${client_id}&refresh_token=${refresh_token}&client_secret=${client_secret}&grant_type=refresh_token`
 
-  const url = `${tokenExchangeUrl}?client_id=${client_id}&refresh_token=${refresh_token}&client_secret=${client_secret}&grant_type=refresh_token`
+  const url = tokenExchangeUrl + params
 
   const options = {
     method: 'POST',
--- backend/functions/getActivitiesList.js
+++ backend/functions/getActivitiesList.js
@@ -3,8 +3,9 @@ const axios = require('axios')
 
 async function getActivitiesList(access_token) {
   let activityList = []
-  let flag = 1
-  let page = 1
+  let flag = 1            // flag to end the loop
+  let page = 1            // start page for the Strava API calls
+  let per_page = 200      // total activities for each page of API Call (200 max)
 
   const options = {
       method: 'GET',
@@ -16,34 +17,39 @@ async function getActivitiesList(access_token) {
 
   // while loop to get all the activities - max 200 per page
   while (flag === 1) {
-    let url = `https://www.strava.com/api/v3/activities/?page=${page}&per_page=200`
+    let url = `https://www.strava.com/api/v3/activities/?page=${page}&per_page=${per_page}`
     try {
       const response = await axios.get(url, options) 
       const { data } = response
       data.forEach(record => {
         const { type, id, name, external_id } = record
-        let flag = 0
+        let add = 0
 
+        // only check for Virtual Rides
         if (type === 'VirtualRide') {
           for (let s = 0; s < name.length; s++) {
+            // if the name contains Zwift - add to the list
             if (name.substr(s, s + 5).toLowerCase() == 'zwift') {
-              flag = 1
+              add = 1
               s = name.length
             }
-           if (external_id.substr(s, s + 5).toLowerCase() == 'zwift') {
-              flag = 1
+            // if the external id contains Zwift - add to the list
+            if (external_id.substr(s, s + 5).toLowerCase() == 'zwift') {
+              add = 1
               s = name.length
             }
           }
         }
         
-        if (flag === 1) activityList.unshift(id)
+        // add activity to the front of the list. 
+        // Zwift calls newest to oldest.  We want to process the oldest first 
+        if (add === 1) activityList.unshift(id)
       });
     
     // move to next page 
     page += 1
     // if the list is not maxed out, end the loop (no activities left)
-    if (data.length < 200) flag = 0
+    if (data.length < per_page) flag = 0
     } catch (error) {
       console.log(error)
       return error
